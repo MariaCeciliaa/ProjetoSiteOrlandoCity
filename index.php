bfbb84a8ecb3fc1a2a80134707a21d04ba838374
MariaCeciliaa <mariacecilia@edu.unifil.br>
2023-07-26 12:05:03 -0300

    Iniciando projeto da loja virtual

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+require_once("inc/configuration.php");
+
+$sql = new Sql();
+
+$result = $sql->query("SELECT * FROM tb_produtos;");
+
+while($row = mysqli_fetch_array($result)){
+
+    var_dump($row);
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
